3480c9a3f9cec7af3eb3f249cad6579b164eb8a7
skaefer143 <stormkaefer@gmail.com>
2017-03-29 13:23:29 -0600

    updated python file name, also now outputs to a file. Phase1 actually complete

---
+++ Phase1/putcodehere.py
@@ -0,0 +1,19 @@
+import sys
+import xml.etree.ElementTree as ET
+
+
+def parseTweets():
+	tree = ET.parse(filename)
+	root = tree.getroot()
+
+	for status in root.findall('status'):
+		id = status.find("id").text
+		print(id + ":" + ET.tostring(status, "unicode"))
+		outputFile.write(id + ":" + ET.tostring(status, "unicode"))
+
+
+filename = input("Please enter filename being used for input: ")
+file = open(filename, "r")
+outputFilename = "tweets.txt"
+outputFile = open(outputFilename, "w")
+parseTweets()
