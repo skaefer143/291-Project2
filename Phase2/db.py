5d886e8e8af8413a951ceff251fbdccc44dfeb66
skaefer143 <stormkaefer@gmail.com>
2017-03-29 19:44:50 -0600

    updated the db.py command so it doesn't overwrite duplicates, and simplified HowToRunCommands

--- Phase2/db.py
+++ Phase2/db.py
@@ -1,13 +1,15 @@
 import subprocess
 
 def dateDB():
-	subprocess.call(["db_load", "-f", dateFilename, "-T", "-t", "btree", "da.idx"]) # -h for sorting by the id key at start, -u for unique entries
+	subprocess.call(["db_load", "-c", "duplicates=1", "-f", dateFilename, "-T", "-t", "btree", "da.idx"]) # -h for sorting by the id key at start, -u for unique entries
 	# Outputs db_load command to da.idx
+	# The -c dupsort=1 argument makes sure that duplicates are in the database and sorted
 	return
 
 def termsDB():
-	subprocess.call(["db_load", "-f", termsFilename, "-T", "-t", "btree", "te.idx"]) # -h for sorting by the id key at start, -u for unique entries
+	subprocess.call(["db_load", "-c", "duplicates=1", "-f", termsFilename, "-T", "-t", "btree", "te.idx"]) # -h for sorting by the id key at start, -u for unique entries
 	# Outputs db_load command to te.idx
+	# The -c dupsort=1 argument makes sure that duplicates are in the database and sorted
 	return
 
 def tweetDB():
@@ -16,9 +18,12 @@ def tweetDB():
 	# Outputs db_load command to tw.idx
 	return
 
-tweetsFilename = input("Please enter filename that has sorted Tweets input: ")
-termsFilename = input("Please enter filename that has sorted Terms input: ")
-dateFilename = input("Please enter filename that has sorted Date input: ")
+print("Filenames must be indexedTweets.txt, indexedTerms.txt, and indexedDates.txt for program to work.")
+print("This message will always display.")
+
+tweetsFilename = "indexedTweets.txt"
+termsFilename = "indexedTerms.txt"
+dateFilename = "indexedDates.txt"
 tweetDB()
 termsDB()
 dateDB()
--- Phase3/dbtest.py
+++ Phase3/dbtest.py
@@ -1,13 +1,38 @@
 from bsddb3 import db 
 #Get an instance of BerkeleyDB 
-database = db.DB() 
-database.open("tw.idx")
+tweetsDatabase = db.DB() 
+tweetsDatabase.open("tw.idx")
+termsDatabase = db.DB() 
+termsDatabase.open("te.idx")
+dateDatabase = db.DB() 
+dateDatabase.open("da.idx")
 
-cur = database.cursor() 
+print("Tweets Database:")
+cur = tweetsDatabase.cursor() 
 iter = cur.first()
 while iter:
 	print(iter[0].decode("utf-8") + "\t" + iter[1].decode("utf-8"))
 	iter = cur.next()
 
 cur.close()
-database.close()
+tweetsDatabase.close()
+
+print("Terms Database:")
+cur = termsDatabase.cursor() 
+iter = cur.first()
+while iter:
+	print(iter[0].decode("utf-8") + "\t" + iter[1].decode("utf-8"))
+	iter = cur.next()
+
+cur.close()
+termsDatabase.close()
+
+print("Date Database:")
+cur = dateDatabase.cursor() 
+iter = cur.first()
+while iter:
+	print(iter[0].decode("utf-8") + "\t" + iter[1].decode("utf-8"))
+	iter = cur.next()
+
+cur.close()
+dateDatabase.close()
