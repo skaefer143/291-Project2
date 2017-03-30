af8fa5e90cd518fadfe36d13a86c8a1c1e68f060
skaefer143 <stormkaefer@gmail.com>
2017-03-29 18:35:38 -0600

    Phase 2 completely done, all intermediate files in repo now

--- Phase2/db.py
+++ Phase2/db.py
@@ -1,10 +1,26 @@
 import subprocess
 
+def dateDB():
+	subprocess.call(["db_load", "-f", dateFilename, "-T", "-t", "btree", "da.idx"]) # -h for sorting by the id key at start, -u for unique entries
+	# Outputs db_load command to da.idx
+	return
+
+def termsDB():
+	subprocess.call(["db_load", "-f", termsFilename, "-T", "-t", "btree", "te.idx"]) # -h for sorting by the id key at start, -u for unique entries
+	# Outputs db_load command to te.idx
+	return
+
 def tweetDB():
 	# With help from https://www.cyberciti.biz/faq/python-execute-unix-linux-command-examples/
-	subprocess.call(["db_load", "-f", filename, "-T", "-t", "hash", "tw.idx"]) # -h for sorting by the id key at start, -u for unique entries
+	subprocess.call(["db_load", "-f", tweetsFilename, "-T", "-t", "hash", "tw.idx"]) # -h for sorting by the id key at start, -u for unique entries
 	# Outputs db_load command to tw.idx
 	return
 
-filename = input("Please enter filename being used for input: ")
+tweetsFilename = input("Please enter filename that has sorted Tweets input: ")
+termsFilename = input("Please enter filename that has sorted Terms input: ")
+dateFilename = input("Please enter filename that has sorted Date input: ")
 tweetDB()
+termsDB()
+dateDB()
+
+exit()
--- Phase2/sort.py
+++ Phase2/sort.py
@@ -1,12 +1,28 @@
 import subprocess
 
+def dateSort():
+	subprocess.call(["sort", "-h", "-u", dateFilename, "--output=sortedDates.txt"]) # -h for sorting by the id key at start, -u for unique entries
+	# Outputs sort command to sortedTweets.txt
+	return
+
+def termSort():
+	subprocess.call(["sort", "-h", "-u", termsFilename, "--output=sortedTerms.txt"]) # -h for sorting by the id key at start, -u for unique entries
+	# Outputs sort command to sortedTweets.txt
+	return
+
 def tweetSort():
 	# With help from https://www.cyberciti.biz/faq/python-execute-unix-linux-command-examples/
-	subprocess.call(["sort", "-h", "-u", filename, "--output=sortedTweets.txt"]) # -h for sorting by the id key at start, -u for unique entries
+	subprocess.call(["sort", "-h", "-u", tweetFilename, "--output=sortedTweets.txt"]) # -h for sorting by the id key at start, -u for unique entries
 	# Outputs sort command to sortedTweets.txt
 	#subprocess.call(["sort", "-h", "-u", filename, "-o"])
 	# Outputs sort command to filename
 	return
 
-filename = input("Please enter filename being used for input: ")
+tweetFilename = input("Please enter filename that has unsorted Tweet input: ")
+termsFilename = input("Please enter filename that has unsorted Terms input: ")
+dateFilename = input("Please enter filename that has unsorted Date input: ")
 tweetSort()
+termSort()
+dateSort()
+
+exit()
