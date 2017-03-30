f8cd23e0e721b55b7f87d3dbacd747f7c18e9b26
skaefer143 <stormkaefer@gmail.com>
2017-03-29 19:06:43 -0600

    fixed sort function, made easier to use.

--- Phase2/sort.py
+++ Phase2/sort.py
@@ -1,26 +1,30 @@
 import subprocess
 
 def dateSort():
-	subprocess.call(["sort", "-h", "-u", dateFilename, "--output=sortedDates.txt"]) # -h for sorting by the id key at start, -u for unique entries
+	subprocess.call(["sort", "-u", dateFilename, "--output=sortedDates.txt"]) # -h for sorting by the id key at start, -u for unique entries
 	# Outputs sort command to sortedTweets.txt
 	return
 
 def termSort():
-	subprocess.call(["sort", "-h", "-u", termsFilename, "--output=sortedTerms.txt"]) # -h for sorting by the id key at start, -u for unique entries
+	subprocess.call(["sort", "-u", termsFilename, "--output=sortedTerms.txt"]) # -h for sorting by the id key at start, -u for unique entries
 	# Outputs sort command to sortedTweets.txt
 	return
 
 def tweetSort():
 	# With help from https://www.cyberciti.biz/faq/python-execute-unix-linux-command-examples/
-	subprocess.call(["sort", "-h", "-u", tweetFilename, "--output=sortedTweets.txt"]) # -h for sorting by the id key at start, -u for unique entries
+	subprocess.call(["sort", "-u", tweetFilename, "--output=sortedTweets.txt"]) # -h for sorting by the id key at start, -u for unique entries
 	# Outputs sort command to sortedTweets.txt
 	#subprocess.call(["sort", "-h", "-u", filename, "-o"])
 	# Outputs sort command to filename
 	return
 
-tweetFilename = input("Please enter filename that has unsorted Tweet input: ")
-termsFilename = input("Please enter filename that has unsorted Terms input: ")
-dateFilename = input("Please enter filename that has unsorted Date input: ")
+print("Filenames must be tweets.txt, terms.txt, and dates.txt for program to work.")
+print("This message will always display.")
+
+
+tweetFilename = "tweets.txt"
+termsFilename = "terms.txt"
+dateFilename = "dates.txt"
 tweetSort()
 termSort()
 dateSort()
